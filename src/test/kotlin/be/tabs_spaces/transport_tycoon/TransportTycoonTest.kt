e75b39ea78c37889cadd0f25e0ac28648768e437
Jan Van Ryckeghem <jan@tabs-spaces.be>
2023-03-17 18:08:40 +0100

    WIP: A-case

--- src/test/kotlin/be/tabs_spaces/transport_tycoon/TransportTycoonTest.kt
+++ src/test/kotlin/be/tabs_spaces/transport_tycoon/TransportTycoonTest.kt
@@ -1,3 +1,6 @@
+import TransportTycoonTest.Location.*
+import TransportTycoonTest.Transporter.Companion.boat
+import TransportTycoonTest.Transporter.Companion.truck
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.CsvSource
 import kotlin.test.assertEquals
@@ -6,10 +9,10 @@ class TransportTycoonTest {
 
     @CsvSource(
         value = [
+            "A,5",
             "BB,5",
             "BBB,15",
             "BBBBB,25",
-//            "A,5",
 //            "AB,5",
 //            "ABB,7",
 //            "AA,13"
@@ -22,49 +25,55 @@ class TransportTycoonTest {
         assertEquals(expected.toInt(), result)
     }
 
-    /**
-     * A=1+4        -> 1 to port, 4 to B
-     * AB=5         -> straight to B
-     * AA=1+4+4+4   -> go to port in parallel -> boat goes from port to A twice + return
-     * BB=5         -> straight
-     */
     private fun transport(input: String): Int {
         val packets = input
-            .map { Destination.valueOf(it.toString()) }
+            .map { Location.valueOf(it.toString()) }
             .map { location -> Packet(location) }
             .toList()
-        val trucks = listOf(Truck(), Truck())
+        val transporters = listOf(truck(), truck(), boat())
 
         var tick = 0
-        while (!packets.delivered()) {
+        while (!packets.delivered() && tick <= 25) {
             packets
                 .filter { it.arrivesAt == tick }
-                .forEach { it.arrived = true }
+                .filter { it.location == it.destination }
+                .forEach {
+                    it.arrived = true
+                }
 
-            trucks.filter { it.availableAt <= tick }
-                .forEach { truck ->
-                    val availablePacket = packets.firstOrNull { it.arrivesAt == null }
+            transporters
+                .filter { it.availableAt <= tick }
+                .forEach { transporter ->
+                    val availablePacket = packets
+                        .filter { it.location == transporter.pickupLocation }
+                        .firstOrNull { it.arrivesAt?.let { it <= tick } ?: true }
 
                     availablePacket?.apply {
                         when (destination) {
-                            Destination.A -> {
+                            A -> {
                                 arrivesAt = tick + 1
-                                truck.availableAt = tick + 2 * 1
+                                location = PORT
+                                transporter.availableAt = tick + 2 * 1
                             }
 
-                            Destination.B -> {
+                            B -> {
                                 arrivesAt = tick + 5
-                                truck.availableAt = tick + 2 * 5
+                                location = destination
+                                transporter.availableAt = tick + 2 * 5
                             }
-
-                            Destination.PORT -> {}
+                            PORT -> {
+                                arrivesAt = tick + 4
+                                location = A
+                                transporter.availableAt = tick + 2 * 4
+                            }
+                            FACTORY -> {}
                         }
                     }
 
                 }
             println(tick)
             println(packets)
-            println(trucks)
+            println(transporters)
             println("---")
             tick++
         }
@@ -73,22 +82,26 @@ class TransportTycoonTest {
 
     private fun List<Packet>.delivered() = all { it.arrived }
 
-    enum class Destination {
+    enum class Location {
         A,
         B,
-        PORT
+        PORT,
+        FACTORY
     }
 
-    data class Truck(
-        var availableAt: Int = 0
-    )
-
-    data class Boat(
-        val destination: Destination
-    )
+    data class Transporter(
+        var availableAt: Int = 0,
+        val pickupLocation: Location
+    ) {
+        companion object {
+            fun truck() = Transporter(pickupLocation = FACTORY)
+            fun boat() = Transporter(pickupLocation = PORT)
+        }
+    }
 
     data class Packet(
-        val destination: Destination,
+        val destination: Location,
+        var location: Location = FACTORY,
         var arrived: Boolean = false,
         var arrivesAt: Int? = null
     )
